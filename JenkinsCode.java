a77bb2eb0060ff53c59c6ecadab7d501c6dc9cd7
unknown <>
2023-10-28 16:14:22 +0530

    Second Commit

--- JenkinsCode.java
+++ JenkinsCode.java
@@ -5,8 +5,13 @@ public class JenkinsCode {
         System.out.println("This code is written for the Jenkins Tutorial.");
         System.out.println("This will be used to test the feature of jenkins.");
         System.out.println("Current Date with time : "+new Date());
+
+        Student student = new Student();
+        student.setName("Unknown");
+        student.setRollNumber("123464565");
+        student.printStudentsDetails();
     }
 } 
-// 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 
+
 
 
---
+++ Student.java
@@ -0,0 +1,18 @@
+public class Student {
+    private String name;
+    private String rollNumber;
+    public Student() {
+        System.out.println("Student Object Created.");
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setRollNumber(String rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public printStudentsDetails() {
+        System.out.println("Student Name : "+name+", Roll Number : "+rollNumber);
+    }
+}
