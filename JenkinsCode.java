444109d778763e239a4f7dd68df0f429eec0b3a6
unknown <>
2023-10-28 15:31:57 +0530

    First Commit

---
+++ JenkinsCode.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class JenkinsCode {
+    public static void main(String[] args) {
+        System.out.println("This code is written for the Jenkins Tutorial.");
+        System.out.println("This will be used to test the feature of jenkins.");
+        System.out.println("Current Date with time : "+new Date());
+    }
+} 
+// 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 
+
+
